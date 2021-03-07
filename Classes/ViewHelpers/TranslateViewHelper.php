1d74d6bc1a2b9ab6eca5f0f7199282d5da219d86
Mati Sediqi <mati_01@icloud.com>
2021-03-07 15:15:06 +0100

    [NEW] TranslateViewHelper

---
+++ Classes/ViewHelpers/TranslateViewHelper.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Site\Frontend\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * This ViewHelper determines whether the given CType has
+ * for its own content element a .css file or not.
+ */
+class TranslateViewHelper extends AbstractViewHelper
+{
+    /**
+     * Initialization of required arguments for this ViewHelper.
+     *
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('extKey', 'string', 'Optional, by default it takes the "FRONTEND_EXT" from the .env file. Extension key provided as snake_case.', false);
+        $this->registerArgument('identifier', 'string', 'The identifier passed as e.g. "Frontend.Test:key".', true);
+    }
+
+    /**
+     * The actual render logic whenever this VH gets called.
+     * Handles the given arguments, generates and adds at the end.
+     *
+     * @return void
+     */
+    public function render()
+    {
+        $extKey = $this->arguments['extKey'] ?? env('FRONTEND_EXT');
+        $identifier = $this->arguments['identifier'];
+
+        $language = request()->getAttribute('language');
+        $twoLetterIsoCode = $language->getTwoLetterIsoCode();
+
+        return ll($extKey, $identifier, $twoLetterIsoCode);
+    }
+}
---
+++ Resources/Private/Language/de/Frontend/Test.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'key' => 'Testing german key',
+];
